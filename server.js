daa66d40abc6bb89cc7635ca01714a72e463b83f
ThatCodingGuy8 <30600514+ThatCodingGuy8@users.noreply.github.com>
2022-04-22 10:59:40 -0400

    Initial

---
+++ server.js
@@ -0,0 +1,26 @@
+/*
+    Branden's Computer Science Create Task Project
+*/
+
+//Config
+const HttpPort = 8344; // The port the webserver will listen on.
+
+//Actual Code
+const express = require("express");
+const server = express();
+const options = {
+    root: __dirname
+}
+
+//GET Handlers
+server.get("/", (req, res) => {
+    res.sendFile("public/html/main.html", options); // Serve main html file
+})
+
+//Middleware
+server.use(express.static("public"))
+
+//Server Initialization
+server.listen(HttpPort, () => {
+    console.log("Server listening on http://localhost:" + HttpPort)
+})
